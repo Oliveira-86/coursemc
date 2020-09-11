8e77e4b356cb348df8433b7b4173e32be8a4b044
Egberto Oliveira <egberto.duarte.14@gmail.com>
2020-09-11 19:03:54 -0300

    Class Category

--- src/main/java/com/edto/cursomc/resources/CategoryResource.java
+++ src/main/java/com/edto/cursomc/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.edto.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edto.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
 	@GetMapping
-	public String listar() {
-		return "lista criada";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.addAll(Arrays.asList(cat1, cat2));
+		
+		return list;
 	}
 
 }
