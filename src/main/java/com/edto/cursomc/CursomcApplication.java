4b32176eb6132e58dcd5582cf8a9ff5f1a381675
Egberto Oliveira <egberto.duarte.14@gmail.com>
2020-09-21 19:39:17 -0300

    Entities State and City

--- src/main/java/com/edto/cursomc/CursomcApplication.java
+++ src/main/java/com/edto/cursomc/CursomcApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.edto.cursomc.domain.Category;
+import com.edto.cursomc.domain.City;
 import com.edto.cursomc.domain.Product;
+import com.edto.cursomc.domain.State;
 import com.edto.cursomc.repositories.CategoryRepository;
+import com.edto.cursomc.repositories.CityRepository;
 import com.edto.cursomc.repositories.ProductRepository;
+import com.edto.cursomc.repositories.StateRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,8 +52,23 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat2));
 		
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+		
+		City cit1 = new City(null, "Urbelândia", st1);
+		City cit2 = new City(null, "São Paulo", st2);
+		City cit3 = new City(null, "Campinas", st2);
+		
+		st1.getCities().addAll(Arrays.asList(cit1));
+		st2.getCities().addAll(Arrays.asList(cit2, cit3));
+		
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(cit1, cit2, cit3));
 		
 	}
 
---
+++ src/main/java/com/edto/cursomc/domain/State.java
@@ -0,0 +1,85 @@
+package com.edto.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class State implements Serializable{
+	private static final long serialVersionUID = 1L; 
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "state")
+	private List<City> cities = new ArrayList<>();
+	
+	public State() {
+	}
+
+	public State(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<City> getCities() {
+		return cities;
+	}
+
+	public void setCities(List<City> cities) {
+		this.cities = cities;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cities == null) ? 0 : cities.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (cities == null) {
+			if (other.cities != null)
+				return false;
+		} else if (!cities.equals(other.cities))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
