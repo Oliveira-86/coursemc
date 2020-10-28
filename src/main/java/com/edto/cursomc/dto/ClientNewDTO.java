ccd86cf25aa61d0a7ddb99153d04fab853a3e9aa
Egberto Oliveira <egberto.duarte.14@gmail.com>
2020-10-28 18:40:17 -0300

    Custom Validation: Email not repeated in client update

--- src/main/java/com/edto/cursomc/dto/ClientNewDTO.java
+++ src/main/java/com/edto/cursomc/dto/ClientNewDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.edto.cursomc.services.validation.ClientInsert;
+
+@ClientInsert
 public class ClientNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/com/edto/cursomc/services/validation/ClientUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.edto.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.edto.cursomc.domain.Client;
+import com.edto.cursomc.dto.ClientDTO;
+import com.edto.cursomc.repositories.ClientRepository;
+import com.edto.cursomc.resources.exceptions.FieldMessage;
+
+public class ClientUpdateValidator implements ConstraintValidator<ClientUpdate, ClientDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClientRepository repository;
+	
+	@Override
+	public void initialize(ClientUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClientDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id")); 
+		
+		Client aux = repository.findByEmail(objDto.getEmail());
+		if(aux.getEmail()!=null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("Email", "Existing Email!"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
