8f34f33173806726d8c28c438a496f0d3254f285
Egberto Oliveira <egberto.duarte.14@gmail.com>
2020-11-14 13:25:28 -0300

    Creating Dev profile

--- src/main/java/com/edto/cursomc/config/DevConfig.java
+++ src/main/java/com/edto/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.edto.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -15,7 +16,15 @@ public class DevConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	public boolean instatiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
